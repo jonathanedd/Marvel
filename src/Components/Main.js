c96882be19e234e98aedfa9422da29e21bf922b5
Jhonatan-Ordonez <jhoeduardonez@gmail.com>
2023-04-20 20:30:37 -0500

    Removed Nav component, and added nav in Main component as a middle nav

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 import "../Styles/main.css";
 import charactersImg from "../Images/charactersImg.jpg";
 import comicsImg from "../Images/comicsImg.jpg";
+import logo from "../Images/logo.png";
 // import mainImg from "../Images/main-img.webp";
 
 //hooks
@@ -10,24 +11,39 @@ import comicsImg from "../Images/comicsImg.jpg";
 //React router dom
 import { Link } from "react-router-dom";
 //icons
+import { AiOutlineSearch } from "react-icons/ai";
 
 const Main = () => {
   // const [search, setSearch] = useState("");
   return (
     <div className="main">
-      <div className="main-img">hola</div>
-      <div className="card">
-        <Link to="/characters">
-          <img src={charactersImg} style={{ width: "150px" }} alt="" />
-          Characters
+      <div className="main-img"></div>
+
+      <div className="upper-bar">
+        <Link to="/">
+          <img src={logo} alt="" />
         </Link>
+        <form action="">
+          <input className="input" type="text" placeholder="Search" />
+          <AiOutlineSearch />
+        </form>
+        <h1 className="main-title">Welcome to the amazing universe of</h1>
       </div>
 
-      <div>
-        <Link to="/comics">
-          <img src={comicsImg} style={{ width: "135px" }} alt="" />
-          Comics
-        </Link>
+      <div className="main-cards">
+        <div className="card">
+          <Link to="/characters">
+            <img src={charactersImg} alt="" />
+            <h3>Characters</h3>
+          </Link>
+        </div>
+
+        <div className="card">
+          <Link to="/comics">
+            <img src={comicsImg} alt="" />
+            <h3>Comics</h3>
+          </Link>
+        </div>
       </div>
     </div>
   );
